344888439a7d59d30962aebe85856759f71e96ce
selene853 <yanxinyi200828@berkeley.edu>
2026-09-07 09:54:44 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 """Entry point for the SF311 Explorer project."""
-def main()
+def main():
    """Run the SF311 Explorer application"""
-  print("SF311 Explorer is ready!")
+   print("SF311 Explorer is ready!")
 
-if _name_=="_main_"
+if __name__=="__main__":
    main()
